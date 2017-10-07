7b5e1315b8292ae4aaa1b067ff3bc4e0d06ac306
Aspasia Beneti <aspasia.beneti@gmail.com>
2017-10-07 17:28:56 +0200

    Initial commit

---
+++ project.clj
@@ -0,0 +1,19 @@
+(defproject org.clojars.dyne/clj-storage "0.1.0"
+  :description "Minimal storage library"
+  :url "https://github.com/PIENews/clj-storage"
+
+  :license {:author "Dyne.org Foundation"
+            :email "foundation@dyne.org"
+            :year 2017
+            :key "gpl-3.0"}
+
+  :dependencies [[org.clojure/clojure "1.8.0"]
+
+                 ;; logs
+                 [com.taoensso/timbre "4.10.0"]
+
+                 ;; mongodb
+                 [com.novemberain/monger "3.1.0"]]
+
+  :source-paths ["src"]
+  :resource-paths ["resources" "test-resources"])
