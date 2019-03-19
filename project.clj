b6b9a13425ed671cba73ac7ac4bca9759904977b
Jaromil <jaromil@dyne.org>
2019-03-19 15:53:16 +0100

    updated to clojure 10
    also fix to run on jdk10
    
    retarget travis builds to openjdk 7-10

--- project.clj
+++ project.clj
@@ -7,7 +7,11 @@
             :year 2017
             :key "gpl-3.0"}
 
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  ;; fix for jdk 10
+  :managed-dependencies [[org.clojure/core.rrb-vector "0.0.14"]
+                       [org.flatland/ordered "1.5.7"]]
+
+  :dependencies [[org.clojure/clojure "1.10.0"]
 
                  ;; logs
                  [com.taoensso/timbre "4.10.0"]
@@ -16,10 +20,10 @@
                  [org.clojars.dyne/monger "3.2.0-SNAPSHOT"]
 
                  ;; file storage
-                 [com.taoensso/nippy "2.13.0"]
+                 [com.taoensso/nippy "2.14.0"]
 ]
 
   :source-paths ["src" "test"]
   :resource-paths ["resources" "test-resources"]
-  :profiles {:dev {:dependencies [[midje "1.8.3"]]
+  :profiles {:dev {:dependencies [[midje "1.9.6"]]
                    :plugins [[lein-midje "3.1.3"]]}})
