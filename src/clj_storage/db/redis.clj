207f923deb84c9acfa4e993b256643d3e02c272b
Aspasia Beneti <aspasia.beneti@gmail.com>
2019-12-30 15:19:53 +0100

    FUTURE WORK: redis aggregates and pagination due to nature of redis

--- src/clj_storage/db/redis.clj
+++ src/clj_storage/db/redis.clj
@@ -72,7 +72,7 @@
                                               (car/mget (:keys query))))))))
 
   (query [database query pagination]
-    ;; TODO: pagination?
+    ;; pagination for redis is only applicable for specific commands check https://christophermcdowell.dev/post/pagination-with-redis/
     (if (spec/valid? :clj-storage.spec/only-key-map query)
       (wcar* (:conn database) (car/get (:key query)))
       (if (spec/valid? :clj-storage.spec/multiple-keys query)
@@ -84,7 +84,7 @@
     (wcar* (:conn database) (car/del (:key item))))
 
   (aggregate [database formula params]
-    )
+    (log/warn "Not implemented yet. Redis aggregates are specific to specific types like sorted sets https://redis.io/commands/zunionstore. Another option is lua scripts like https://github.com/ptaoussanis/carmine#lua."))
 
   (add-index [database index params]
     (spec/assert ::index-params params)
